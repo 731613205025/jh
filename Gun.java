f9c21a9d53a6a73ecf51cb991f7e0e03dc294ccc
731613205025 <maniharish95@gmail.com>
2016-07-18 13:57:58 +0530

    Add files via upload

---
+++ Gun.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gun;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Gun {
+
+    public static void main(String[] args) {
+           Scanner a=new Scanner(System.in);
+        String n=a.nextLine();
+        int b=Integer.parseInt(n);
+        System.out.println(b);
+        
+    }
+} 
+    
